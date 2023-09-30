59ec676d5627858d37ea0c9700b212ae85134f15
soonhong99 <nanayah99@naver.com>
2023-09-30 19:12:32 +0900

    feat: main.java에서 drawer하면 프로필, 채팅방이 보이는 navigation bar 구현 (미완)

--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -1,28 +1,94 @@
 package com.example.registerloginexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 서버에서 받는 response 인 messege와 access_token을 loginactivity에서 가지고 온다.
-    private TextView res_message, res_access_token;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    // 서버에서 받은 response 인 messege와 access_token을 loginactivity에서 가지고 온다.
+    // private TextView res_message, res_access_token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        res_message = findViewById(R.id.res_message);
-        res_access_token = findViewById(R.id.res_access_token);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.drawer);
 
+        Button btn_open = (Button)findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 네비게이션 메뉴가 열리는 코드
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        Button btn_close = (Button)findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
+        // 아래는 login activity에서 token 값과 messege를 얻기 위해 필요한 코드들 (지우지 말기)
         Intent intent = getIntent();
+
+        // 서버의 response인 messege와 access_token을 저장하는 변수값 생성
         String message = intent.getStringExtra("message");
         String access_token = intent.getStringExtra("access_token");
 
-        res_message.setText(message);
-        res_access_token.setText(access_token);
+        // 해당 messege와 access_token을 화면에 띄울 필요가 없으므로 주석처리 (xml과 연결하는 코드 필요 없음)
+        // res_message = findViewById(R.id.res_message);
+        // res_access_token = findViewById(R.id.res_access_token);
+        // res_message.setText(message);
+        // res_access_token.setText(access_token);
     }
+
+
+    // drawer layout을 오른쪽이나 왼쪽으로 슬라이드 했을 때 이곳에서 상태값을 받아온다.
+    // 추가 기능 구현 할 때 함수안에 적으면 된다.
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+
+
 }
