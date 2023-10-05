01e396d332bcc146a9b62e3ac397c8964f26d7da
gank564 <99009399+gank564@users.noreply.github.com>
2023-10-05 18:53:08 +0900

    feat : GPS 구현

--- app/src/main/java/com/example/registerloginexample/MainActivity.java
+++ app/src/main/java/com/example/registerloginexample/MainActivity.java
@@ -1,14 +1,122 @@
 package com.example.registerloginexample;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.example.registerloginexample.databinding.ActivityMapsBinding;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    private double longitude, latitude;
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private double cur_lat;
+    private double cul_lon;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//    현재 위치 받아오기
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            return;
+        }
+
+        Location loc_Current =
+                locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (loc_Current != null){
+        cur_lat = loc_Current.getLatitude(); //위도
+        cul_lon = loc_Current.getLongitude(); //경도
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                3000, -1, locationListener); // 3초 마다 위치 갱신
+}
+//        지도 표시
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+//    위치 권한 설정
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 100) {
+            // 권한 요청
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,  android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION,  android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            }
+        }
+    }
+
+
+//    마커로 위치 표시
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        LatLng mylocation = new LatLng(cur_lat,cul_lon);
+        // 반경 1KM원
+        CircleOptions circle1KM = new CircleOptions().center(mylocation) //원점
+                .radius(10000)      //반지름 단위 : m
+                .strokeWidth(0f)  //선너비 0f : 선없음
+                .fillColor(Color.parseColor("#880000ff")); //배경색
+        mMap.addMarker(new MarkerOptions().position(mylocation).title("당신의 위치"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
+        //원추가
+        this.mMap.addCircle(circle1KM);
+    }
+}
+
+
+
+class MainActivity2 extends AppCompatActivity {
+
 
     private TextView tv_id, tv_pass;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +129,6 @@ public class MainActivity extends AppCompatActivity {
         String userID = intent.getStringExtra("userID");
         String userPass = intent.getStringExtra("userPass");
 
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
+
     }
 }
